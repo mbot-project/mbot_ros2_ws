80c5d43f537847bbf690cb3f4d76becbad97bc51
Shaw <sunxycn98@gmail.com>
2025-08-21 19:00:01 -0400

    update the mapping process

--- mbot_navigation/launch/slam_toolbox_online_async_launch.py
+++ mbot_navigation/launch/slam_toolbox_online_async_launch.py
@@ -8,7 +8,7 @@ from launch.conditions import IfCondition
 from launch.events import matches_action
 from launch.substitutions import (AndSubstitution, LaunchConfiguration,
                                   NotSubstitution)
-from launch_ros.actions import LifecycleNode
+from launch_ros.actions import LifecycleNode, Node
 from launch_ros.event_handlers import OnStateTransition
 from launch_ros.events.lifecycle import ChangeState
 from lifecycle_msgs.msg import Transition
@@ -19,6 +19,10 @@ def generate_launch_description():
     use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager")
     use_sim_time = LaunchConfiguration('use_sim_time')
     slam_params_file = LaunchConfiguration('slam_params_file')
+    
+    rviz_config_file = os.path.join(
+        get_package_share_directory('mbot_navigation'),
+        'rviz', 'mapping.rviz')
 
     declare_autostart_cmd = DeclareLaunchArgument(
         'autostart', default_value='true',
@@ -36,6 +40,13 @@ def generate_launch_description():
         default_value=os.path.join(get_package_share_directory("mbot_navigation"), # edited for mbot_navigation
                                    'config', 'slam_toolbox_online_async.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
+
+    start_rviz_cmd = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file])
 
     start_async_slam_toolbox_node = LifecycleNode(
         parameters=[
@@ -85,5 +96,6 @@ def generate_launch_description():
     ld.add_action(start_async_slam_toolbox_node)
     ld.add_action(configure_event)
     ld.add_action(activate_event)
+    ld.add_action(start_rviz_cmd)
 
     return ld
