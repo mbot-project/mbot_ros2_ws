417382113871e6db1ca0ed527e9c48a86a3c8be4
Shaw <sunxycn98@gmail.com>
2025-09-10 19:40:05 -0400

    added projection node to test calibration result

--- mbot_vision/setup.py
+++ mbot_vision/setup.py
@@ -24,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'calibration_marker = mbot_vision.calibration_marker:main',
         ],
     },
 )
